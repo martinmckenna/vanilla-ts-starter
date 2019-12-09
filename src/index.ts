581d48a9ece8f339e95ebd7d5a690054e565d81f
mmckenna <mmckenna.phila@gmail.com>
2019-12-08 20:36:23 -0500

    updates loaders

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-function component() {
+import './styles/index.css'
+
+(() => {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = 'Hello world';
+  document.body.appendChild(element);
+})()
 
-  return element;
-}
-
-document.body.appendChild(component());
